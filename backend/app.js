8116dd10f811cecdbdcabbddb8c50c06caafd8de
nightmarelullaby <videotuto32@gmail.com>
2023-09-12 20:13:58 -0400

    set cors origin only to localhost

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cookieParser())
 
 app.use(cors({
-  origin:["http://localhost:3000","https://web-chat-app-kappa.vercel.app"],
+  origin:"http://localhost:3000",
   credentials: true,
 }))
 
