6ed3fc98d40169ef479a5b66f2a94f72f5be599b
nightmarelullaby <videotuto32@gmail.com>
2023-09-17 04:17:55 -0400

    added image get route

--- backend/routes/upload-files.js
+++ backend/routes/upload-files.js
@@ -28,6 +28,28 @@ const storage = new GridFsStorage({
 
 const upload = multer({ storage });
 
+router.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
 router.post('/upload-image',upload.single('image'),(req,res) => {
     return res.json({message:"image uploaded"})
 })
