06393a40867295584f92a737bb72e50392cc045c
nightmarelullaby <videotuto32@gmail.com>
2023-09-17 04:05:30 -0400

    added cookies

--- chat-frontend/src/app/api/upload-image/route.js
+++ chat-frontend/src/app/api/upload-image/route.js
@@ -1,8 +1,16 @@
 import { NextResponse } from "next/server";
-
+import {cookies} from "next/headers";
+    
 export async function POST(req,res){
+    const cookieStore = cookies();
+    const {value} = cookieStore.get("token");
+    
     const formData = await req.formData()
-
+    let headersList = {
+        "Accept": "*/*",
+        "Cookie":"token="+value,
+        "Content-Type": "multipart/form-data",
+    }
     let response = await fetch(process.env.LOCAL_BACKEND+"/api/upload-image", { 
         method: "POST",
         headers: headersList,
