263eb462e9d915234bec38610c72cf89e1d3db3e
nightmarelullaby <videotuto32@gmail.com>
2023-10-11 01:54:21 -0400

    added middleware fix

--- chat-frontend/src/middleware.ts
+++ chat-frontend/src/middleware.ts
@@ -1,7 +1,26 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import {getUserInformation} from '@/services/getUserInformation';
- 
+ import {cookies} from "next/headers";
+
+export const validateUser = async (token) => {
+  let headersList = {
+    "Accept": "*/*",
+    "Cookie":`token=${token}`,
+    "Content-Type": "application/json"
+  }
+let response = await fetch(process.env.LOCAL_BACKEND+ "/api/auth/verify", { 
+  method: "GET",
+  headers: headersList
+});
+if(response.status !== 200){
+    const {message} = await response.json()
+    throw new Error("error!",message)
+  } 
+  const responseParsed = await response.json()
+  return responseParsed
+}
+
+
 export async function middleware(req: NextRequest) {
   const access_token = req.cookies.get("token")
   if(!access_token){
@@ -10,15 +29,18 @@ export async function middleware(req: NextRequest) {
     url.pathname = `/auth/login`;
     return NextResponse.redirect(url);
   }
-  try{
-    const tokenValidation = await getUserInformation()
+  if(access_token){
+    try{
+    const tokenValidation = await validateUser(access_token.value)
     return NextResponse.next();
-  }catch(error){
-    const requestedPage = req.nextUrl.pathname;
-    const url = req.nextUrl.clone();
-    url.pathname = `/auth/login`;
-    return NextResponse.redirect(url);
+    } catch(error){
+      const requestedPage = req.nextUrl.pathname;
+      const url = req.nextUrl.clone();
+      url.pathname = `/auth/login`;
+      return NextResponse.redirect(url);
+    }  
   }
+  
 
 }
 // See "Matching Paths" below to learn more
--- chat-frontend/src/services/auth/loginUser.js
+++ chat-frontend/src/services/auth/loginUser.js
@@ -1,27 +1,20 @@
 import Cookies from "js-cookie"
 export const loginUser = async (data) => {
-	
 	const bodyContent = data
-	const fetchBody = {
-
-	}
 	const URL = "https://chat-backend-r4ns.onrender.com/api/auth/login"
-
 		const registerRequest = await fetch(URL , {
 		method: 'POST',
-		// credentials: 'include', 
 		headers: {
       		'Content-Type': 'application/json'
     	},
     	body: JSON.stringify(bodyContent)
 	})
+
 	if(registerRequest.status !== 200){
-		const error = await registerRequest.json()
-		console.log(error)
+		const error = await registerRequest.json()		
 		throw new Error(error.message)
 	}	
 	const responseParsed = await registerRequest.json()
-	console.log(responseParsed)
 	Cookies.set('token', responseParsed.token,{ expires: 365})
 	return responseParsed
 }
