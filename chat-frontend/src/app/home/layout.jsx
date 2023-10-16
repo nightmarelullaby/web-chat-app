b605f55c93471797c84985cc59093e1a160b8713
nightmarelullaby <videotuto32@gmail.com>
2023-10-15 21:20:45 -0400

    home design improved

--- chat-frontend/src/app/home/layout.jsx
+++ chat-frontend/src/app/home/layout.jsx
@@ -1,8 +1,8 @@
 import {Box,Flex} from "@/components/chakra-client/components"
 import Link from "next/link"
 export default function HomeLayout({children}){
-   return <><Box as="header">
-                  <Flex as="nav">
+   return <><Box as="header" py="4">
+                  <Flex as="nav" justifyContent="center" gap="4">
                     <Link href="/home">Home</Link>
                     <Link href="/auth/login">Login</Link>
                   </Flex>
--- chat-frontend/src/app/home/page.js
+++ chat-frontend/src/app/home/page.js
@@ -1,24 +1,27 @@
 import Image from 'next/image'
 import Link from "next/link"
-import {Center,Text,VStack,Button} from "@/components/chakra-client/components"
+import {Center,Flex,Text,VStack,Button,Img} from "@/components/chakra-client/components"
 export default function Home() {
   return (
-    <Center h="100vh"  background="repeating-linear-gradient(90deg, #ececee 0, #ececec .4%, transparent 0, transparent 50%),repeating-linear-gradient(180deg, #ffffff 0, #ececec .4%, #ffffff00 0, transparent 50%)"
-        backgroundSize="12em 12em"
-        bgColor="white"
-        opacity={1}>
+    <Center h="100vh">
 
-    <VStack gap="0" alignItems="stretch">
-      <Text fontSize="6xl" as="h1" fontFamily="system-ui"color="gray.800" letterSpacing="-.04em" fontWeight="700">
-          Fast, flexible and secure
+    <VStack alignItems="center" px={{base:"8px",sm:"16px",md:0}}>
+    <Img src="/images/sammy-line-man-receives-a-mail.png" objectFit="cover" h={{base:"230px",sm:"300px"}} w={{base:"230px",sm:"300px"}}/>
+      <Text textAlign="center" fontSize={{base:"3xl",sm:"5xl",md:"5xl"}} as="h1" color="gray.800" letterSpacing="-.04em" fontWeight="700">
+          The new way to connect with all your people
       </Text>
-        <Text textAlign="center" fontSize="xl" as="p" color="gray.600" fontWeight="400">
-          The new way to keep connected with your friends is here
+        <Text textAlign="center" fontSize="xl" as="p" color="gray.600" fontWeight="300">
+          The new way to keep connected with your friends is here!
       </Text>
       <Link href="/auth/login" style={{alignSelf:"center"}}>
-      <Button  py="6" px="8" fontFamily="system-ui" mt="4" bg="gray.50" borderColor="gray.300" transition="0s" borderWidth="1px" fontWeight="600" letterSpacing=".4px" color="gray.700" fontSize="md">
+      <Flex gap={{base:"6px",sm:"6px",md:"2"}} direction={{base:"column",sm:"row"}}>
+      <Button _hover={{bg:"cyan.700"}} bg="gray.800"  borderRadius="200px" borderTopRightRadius={{base:200,sm:"0" }} borderBottomRightRadius={{base:200,sm:"0" }}  py="6" px="8" fontWeight="500" letterSpacing=".4px" color="white" fontSize="sm">
         Register now for free!
       </Button>
+       <Button  bg="white" borderWidth=".5px" borderColor="gray.400" borderRadius="200px" borderTopLeftRadius={{base:200,sm:"0" }} borderBottomLeftRadius={{base:200,sm:"0" }}py="6" px="8"  fontWeight="400" letterSpacing=".4px" color="gray.700" fontSize="sm">
+        Sign in
+      </Button>
+      </Flex>
       </Link>
       </VStack>
     </Center>
